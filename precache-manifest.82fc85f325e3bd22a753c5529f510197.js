60a6770cc152491e7b391a6ddd4fc09f73d32857
rockingatgithub <contactwithsid@gmail.com>
2020-10-21 14:19:35 +0530

    Updates

---
+++ precache-manifest.82fc85f325e3bd22a753c5529f510197.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03e1b80bd02c68acabe41829bf02feed",
+    "url": "/aakar/index.html"
+  },
+  {
+    "revision": "c9384c2b53490c9937e3",
+    "url": "/aakar/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "728863570d1d213988bf",
+    "url": "/aakar/static/css/main.5875c8e0.chunk.css"
+  },
+  {
+    "revision": "c9384c2b53490c9937e3",
+    "url": "/aakar/static/js/2.83970b29.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/aakar/static/js/2.83970b29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "728863570d1d213988bf",
+    "url": "/aakar/static/js/main.aa4c24f3.chunk.js"
+  },
+  {
+    "revision": "a4aee14b69d8019d0969",
+    "url": "/aakar/static/js/runtime-main.3036bde6.js"
+  },
+  {
+    "revision": "196d22a4b1b63023003458920da0e399",
+    "url": "/aakar/static/media/Rectangle_11.196d22a4.png"
+  },
+  {
+    "revision": "d50c01ed58fc7e7059c3879b6285cc48",
+    "url": "/aakar/static/media/Rectangle_12.d50c01ed.png"
+  },
+  {
+    "revision": "e1bb62982930d52e01f1388bba48f727",
+    "url": "/aakar/static/media/Rectangle_13.e1bb6298.png"
+  },
+  {
+    "revision": "c3acc570a656686f12eade714a51d57a",
+    "url": "/aakar/static/media/Rectangle_14.c3acc570.png"
+  },
+  {
+    "revision": "46f7d1bf82282e89d410376396529869",
+    "url": "/aakar/static/media/Rectangle_15.46f7d1bf.png"
+  },
+  {
+    "revision": "d744e034bf3b91c423e5fe4c60c1343d",
+    "url": "/aakar/static/media/Rectangle_16.d744e034.png"
+  },
+  {
+    "revision": "d147ee4911685ca164a16eb5097e0d45",
+    "url": "/aakar/static/media/knowimage2.d147ee49.png"
+  },
+  {
+    "revision": "ef6801a8ee7e3e9a94245f01952badf1",
+    "url": "/aakar/static/media/knowimage3.ef6801a8.png"
+  },
+  {
+    "revision": "b2e89729cf7004e93ad1b2072f360212",
+    "url": "/aakar/static/media/knowimage4.b2e89729.png"
+  },
+  {
+    "revision": "c60fb2fceccf9bf2fd73071b97147c54",
+    "url": "/aakar/static/media/knowimage5.c60fb2fc.png"
+  },
+  {
+    "revision": "4082d542f4d636b06a4ef512af8a167f",
+    "url": "/aakar/static/media/knowimage6.4082d542.png"
+  },
+  {
+    "revision": "8b3f11f5e377be96a9d0cbcb3f001f40",
+    "url": "/aakar/static/media/knowimage7.8b3f11f5.png"
+  },
+  {
+    "revision": "290696f01da386adbea3c79b70aec7c1",
+    "url": "/aakar/static/media/knowimage8.290696f0.png"
+  },
+  {
+    "revision": "2712a77823d18366591ea5e887ec1d26",
+    "url": "/aakar/static/media/knowyourselflogo.2712a778.png"
+  },
+  {
+    "revision": "ad93d8f5873f77416a48cc9d438a84c2",
+    "url": "/aakar/static/media/moodbooster0.ad93d8f5.png"
+  },
+  {
+    "revision": "9ac1d5bee9a350bda012e763b9af15fe",
+    "url": "/aakar/static/media/moodbooster1.9ac1d5be.png"
+  },
+  {
+    "revision": "8e6be5f168e2c0b8252f41aee9e2700f",
+    "url": "/aakar/static/media/moodbooster2.8e6be5f1.png"
+  },
+  {
+    "revision": "95541cd591314d0b4571ab11166786f8",
+    "url": "/aakar/static/media/moodbooster3.95541cd5.png"
+  },
+  {
+    "revision": "591333f52fe15627003eef38d878aece",
+    "url": "/aakar/static/media/moodbooster4.591333f5.png"
+  },
+  {
+    "revision": "cc83d82c0daf0235defece32e68c218f",
+    "url": "/aakar/static/media/selfcare.cc83d82c.png"
+  }
+]);
