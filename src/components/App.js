5b0ed69aa5c7689ecabecb8ab5c278b9f13f97b9
rockingatgithub <contactwithsid@gmail.com>
2020-10-21 14:08:11 +0530

    all pages added

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+import { Dropdown, Col, Row, Container } from "react-bootstrap";
 import NavbarOne from "./NavbarOne";
 import NavbarTwo from "./NavbarTwo";
 import NavbarThree from "./NavbarThree";
-import NavbarFour from "./NavbarFour";
 import CarouselBody from "./CarouselBody";
 import ImageLayer1 from "./ImageLayer1";
 import VideoLayer from "./VideoLayer";
@@ -11,23 +11,181 @@ import BottomBlocks from "./BottomBlocks";
 import LastRow from "./LastRow";
 import KnowYourself from "./KnowYourself";
 import MoodBoosters from "./MoodBoosters";
+import Blogs from "./Blogs";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      home: true,
+      know: false,
+      mood: false,
+      blogs: false,
+    };
+  }
+
+  knowHandler = () => {
+    this.setState({
+      home: false,
+      know: true,
+      mood: false,
+      blogs: false,
+    });
+  };
+
+  moodHandler = () => {
+    this.setState({
+      home: false,
+      know: false,
+      mood: true,
+      blogs: false,
+    });
+  };
+
+  blogHandler = () => {
+    this.setState({
+      home: false,
+      know: false,
+      mood: false,
+      blogs: true,
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
         <NavbarOne />
         <NavbarTwo />
         <NavbarThree />
-        <NavbarFour />
-        <CarouselBody />
-        <ImageLayer1 />
-        <VideoLayer />
-        <Specialist />
-        <BottomBlocks />
+        <Container fluid className="my-container container-4">
+          <Row noGutters={true}>
+            <Col md={1}></Col>
+            <Col md={2}>
+              <Dropdown>
+                <Dropdown.Toggle
+                  variant="secondary"
+                  id="dropdown-basic"
+                  className="btn-edit"
+                  onClick={this.knowHandler}
+                >
+                  Know Yourself Better
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col md={2}>
+              <Dropdown>
+                <Dropdown.Toggle
+                  variant="secondary"
+                  id="dropdown-basic"
+                  className="btn-edit"
+                  onClick={this.moodHandler}
+                >
+                  Mood Lifters & Boosters
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col md={2}>
+              <Dropdown>
+                <Dropdown.Toggle
+                  variant="secondary"
+                  id="dropdown-basic"
+                  className="btn-edit"
+                  onClick={this.blogHandler}
+                >
+                  Exam Fear & Phobia
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col md={2}>
+              <Dropdown>
+                <Dropdown.Toggle
+                  variant="secondary"
+                  id="dropdown-basic"
+                  className="btn-edit"
+                >
+                  Book Your Therapist
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col md={2}>
+              <Dropdown>
+                <Dropdown.Toggle
+                  variant="secondary"
+                  id="dropdown-basic"
+                  className="btn-edit"
+                >
+                  Blog & Articles
+                </Dropdown.Toggle>
+
+                <Dropdown.Menu>
+                  <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                  <Dropdown.Item href="#/action-2">
+                    Another action
+                  </Dropdown.Item>
+                  <Dropdown.Item href="#/action-3">
+                    Something else
+                  </Dropdown.Item>
+                </Dropdown.Menu>
+              </Dropdown>
+            </Col>
+            <Col md={1} style={{ alignSelf: "center" }}>
+              FAQ'S
+            </Col>
+          </Row>
+        </Container>
+        {this.state.home && (
+          <React.Fragment>
+            <CarouselBody />
+            <ImageLayer1 />
+            <VideoLayer />
+            <Specialist />
+            <BottomBlocks />
+          </React.Fragment>
+        )}
+        {this.state.know && <KnowYourself />}
+        {this.state.mood && <MoodBoosters />}
+        {this.state.blogs && <Blogs />}
         <LastRow />
-        <KnowYourself />
-        <MoodBoosters />
       </React.Fragment>
     );
   }
---
+++ src/components/Blogs.js
@@ -0,0 +1,125 @@
+import React, { Component } from "react";
+import {
+  Container,
+  Row,
+  Col,
+  Form,
+  FormControl,
+  Button,
+  Dropdown,
+  DropdownButton,
+  Card,
+} from "react-bootstrap";
+
+class Blogs extends Component {
+  render() {
+    return (
+      <Container fluid className="blogcontainer">
+        <Row>
+          <Col lg={3}>
+            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+              <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+              <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+              <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton>
+          </Col>
+          <Col lg={6}></Col>
+          <Col lg={3}>
+            <Form inline>
+              <FormControl
+                type="text"
+                placeholder="Search"
+                className="mr-sm-2"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Col>
+        </Row>
+        <Row className="blogsrow">
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col>
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src="holder.js/100px180" />
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Blogs;
